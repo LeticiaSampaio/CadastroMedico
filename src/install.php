e18e3d52b61b5242312ea8fae4c1576569d61102
Leticia Sampaio <leticia.sampaio@prodigious.com>
2020-11-25 20:57:02 -0300

    Configuração inicial do BD.

--- src/install.php
+++ src/install.php
@@ -1,10 +1,10 @@
 <?php
 
-require "src/model/config-banco-dados.php";
+require "model/config-banco-dados.php";
 
 try {
     $connection = new PDO("mysql:host=$host", $username, $password, $options);
-    $sql = file_get_contents("banco-dados/init.sql");
+    $sql = file_get_contents("../banco-dados/init.sql");
     $connection->exec($sql);
   
     echo "Database and table users created successfully.";
--- src/model/config-banco-dados.php
+++ src/model/config-banco-dados.php
@@ -5,7 +5,16 @@
 * Veja o exemplo:
 */
 
-define("DB_HOST", "localhost");
-define("DB_NAME", "teste_fc");
-define("DB_USER", "root");
-define("DB_PASS", "");
+// define("DB_HOST", "localhost");
+// define("DB_NAME", "teste_fc");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+
+$host       = "localhost";
+$username   = "root";
+$password   = "root";
+$dbname     = "test_fc"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
