370a4ac387da57ecd58d67613877e8d2efcab52d
Leticia Sampaio <leticia.sampaio@prodigious.com>
2020-12-01 00:36:45 -0300

    Form com validação front-end.

--- src/view/editar-cadastro-medico.php
+++ src/view/editar-cadastro-medico.php
@@ -6,12 +6,12 @@
     <form class="div_form">
         <h2>Editar médico</h2>
         <label for="name" class="label_form">Nome</label>
-        <input id="name" name="name" class="input_form" placeholder="Nome">
+        <input id="name" name="name" class="input_form" placeholder="Nome" required>
         <label for="senha_antiga" class="label_form">Senha antiga</label>
-        <input type="password" id="senha_antiga" name="senha_antiga" class="input_form" placeholder="Insira a senha antiga">
+        <input type="password" id="senha_antiga" name="senha_antiga" class="input_form" placeholder="Insira a senha antiga" required> 
         <label for="nova_senha" class="label_form">Nova senha</label>
-        <input type="password" id="nova_senha" name="nova_senha" class="input_form" placeholder="Escolha uma nova senha forte e segura">
-        <submit type="submit" name="submit" value="submit" class="button button_confirm">Atualizar cadastro</submit>
+        <input type="password" id="nova_senha" name="nova_senha" class="input_form" placeholder="Escolha uma nova senha forte e segura" required>
+        <input type="submit" name="submit" class="button button_confirm" value="Atualizar cadastro">
         <a href="../index.php" class="link_back">Voltar para a Página inicial</a>
     </form>
 </div>
